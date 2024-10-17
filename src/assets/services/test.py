a529d5998ddd8e3132b1be49521a1915f50b27e2
ponczuTM <mroczkowskioliwer10@gmail.com>
2024-10-17 14:38:22 +0200

    test

--- src/assets/services/test.py
+++ src/assets/services/test.py
@@ -1,6 +1,37 @@
 import win32print
 import win32ui
 from PIL import Image, ImageWin
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+image_path = 'img.png'
+
+pdf_path = 'output.pdf'
+
+def generate_pdf(image_path, pdf_path, text):
+    c = canvas.Canvas(pdf_path, pagesize=letter)
+    width, height = letter  
+
+    img = Image.open(image_path)
+    img_width, img_height = img.size
+
+    img_ratio = img_width / img_height
+    page_ratio = width / height
+
+    if img_ratio > page_ratio:
+        new_width = width
+        new_height = width / img_ratio
+    else:
+        new_height = height
+        new_width = height * img_ratio
+
+    c.drawImage(image_path, 0, height - new_height, width=new_width, height=new_height)
+
+    c.drawString(0, height - new_height - 20, text)  
+
+    c.save()
+
+generate_pdf(image_path, pdf_path, "test")
 
 printer_name = win32print.GetDefaultPrinter()
 
@@ -9,28 +40,21 @@ if not printer_name:
 else:
     print(f"Znaleziono drukarkę: {printer_name}")
 
-    image_path = 'img.png'
     try:
-        img = Image.open(image_path)
+
+        hdc = win32ui.CreateDC()
+        hdc.CreatePrinterDC(printer_name)
+
+        hdc.StartDoc(pdf_path)
+        hdc.StartPage()
+
+        import win32api
+        win32api.ShellExecute(0, "print", pdf_path, None, ".", 0)
+
+        hdc.EndPage()
+        hdc.EndDoc()
+        hdc.DeleteDC()
+
+        print("PDF został wydrukowany.")
     except Exception as e:
-        print(f"Nie udało się otworzyć obrazu: {e}")
-        exit(1)
-
-    hdc = win32ui.CreateDC()
-    hdc.CreatePrinterDC(printer_name)
-
-    hdc.StartDoc(image_path)
-    hdc.StartPage()
-
-    width, height = img.size
-    dib = ImageWin.Dib(img)
-
-    x, y = 0, 0
-
-    dib.draw(hdc.GetHandleOutput(), (x, y, width, height))
-
-    hdc.EndPage()
-    hdc.EndDoc()
-    hdc.DeleteDC()
-
-    print("Obraz został wydrukowany.")
+        print(f"Nie udało się wydrukować PDF: {e}")

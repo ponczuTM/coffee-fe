cd622942119d6b29f09ba341dd7fa99b83ba3414
ponczuTM <mroczkowskioliwer10@gmail.com>
2024-10-17 14:48:29 +0200

    test

--- src/assets/services/works.py
+++ src/assets/services/works.py
@@ -2,12 +2,19 @@ import win32print
 import win32ui
 from PIL import Image, ImageWin
 
-def print_image(image_path, printer_name):
+printer_name = win32print.GetDefaultPrinter()
+
+if not printer_name:
+    print("Nie znaleziono drukarki.")
+else:
+    print(f"Znaleziono drukarkę: {printer_name}")
+
+    image_path = 'img.png'
     try:
         img = Image.open(image_path)
     except Exception as e:
         print(f"Nie udało się otworzyć obrazu: {e}")
-        return
+        exit(1)
 
     hdc = win32ui.CreateDC()
     hdc.CreatePrinterDC(printer_name)
@@ -26,15 +33,4 @@ def print_image(image_path, printer_name):
     hdc.EndDoc()
     hdc.DeleteDC()
 
-    print(f"Obraz '{image_path}' został wydrukowany.")
-
-printer_name = win32print.GetDefaultPrinter()
-
-if not printer_name:
-    print("Nie znaleziono drukarki.")
-else:
-    print(f"Znaleziono drukarkę: {printer_name}")
-
-    print_image('img.png', printer_name)
-
-    print_image('img2.png', printer_name)
+    print("Obraz został wydrukowany.")

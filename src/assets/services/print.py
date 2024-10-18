e9f208e9167684edb585890d873703406af4ed5e
ponczuTM <mroczkowskioliwer10@gmail.com>
2024-10-18 09:28:27 +0200

    1.2

--- src/assets/services/print.py
+++ src/assets/services/print.py
@@ -22,15 +22,30 @@ def setup_database():
     conn.commit()
     conn.close()
 
+# def generate_qr_code(data):
+#     qr = qrcode.QRCode(version=1, box_size=10, border=5)
+#     qr.add_data(data)  
+#     qr.make(fit=True)
+
+#     img = qr.make_image(fill='black', back_color='white')
+#     qr_image_path = 'qr_code.png'
+#     img.save(qr_image_path)
+#     return qr_image_path
+
 def generate_qr_code(data):
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
     qr.add_data(data)  
     qr.make(fit=True)
 
     img = qr.make_image(fill='black', back_color='white')
+    
+    new_size = (int(img.size[0] * 1.2), int(img.size[1] * 1.2))
+    img = img.resize(new_size, Image.NEAREST)
+
     qr_image_path = 'qr_code.png'
     img.save(qr_image_path)
     return qr_image_path
+
 
 def print_image(image_path, printer_name):
     try:
